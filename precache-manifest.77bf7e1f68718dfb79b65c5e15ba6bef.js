fa4d2e0e7d18725988632846610d5213496cb071
NIR <Nirt04@gmail.com>
2019-12-01 17:38:33 +0200

    Updates

---
+++ precache-manifest.77bf7e1f68718dfb79b65c5e15ba6bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09a461fed2af0a24383158a4ae2b5d1",
+    "url": "/MealsPlan/index.html"
+  },
+  {
+    "revision": "d7554b9ed7968a9e4489",
+    "url": "/MealsPlan/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "4ed44df9621c0a64ce13",
+    "url": "/MealsPlan/static/css/main.d16cff7d.chunk.css"
+  },
+  {
+    "revision": "d7554b9ed7968a9e4489",
+    "url": "/MealsPlan/static/js/2.36cf65c8.chunk.js"
+  },
+  {
+    "revision": "4ed44df9621c0a64ce13",
+    "url": "/MealsPlan/static/js/main.1c461414.chunk.js"
+  },
+  {
+    "revision": "b516387a826a16cdbd0d",
+    "url": "/MealsPlan/static/js/runtime-main.94f96ace.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/MealsPlan/precache-manifest.77bf7e1f68718dfb79b65c5e15ba6bef.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MealsPlan/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
